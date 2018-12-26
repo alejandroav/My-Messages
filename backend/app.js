dfb7dd3e767d3ef7d9d9aba81f0fe2d677e30e9c
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2018-12-26 15:28:03 +0100

    Upload

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+const path = require("path");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://root:" + process.env.MONGO_ATLAS_PW + "@cluster0-fdi1a.mongodb.net/mean-course?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Error connecting to database!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts",postsRoutes);
+app.use("/api/user",userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
+
+module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://nodeangular-env.gtqenyupee.us-west-2.elasticbeanstalk.com/api'
 };
